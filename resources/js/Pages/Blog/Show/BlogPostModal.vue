90b63e894b43dfff1da8937250781de5b78362ab
lucaaeberhard <58259302+lucaaeberhard@users.noreply.github.com>
2021-07-06 09:59:17 +0200

    Update BlogPostModal.vue
    Like finished

--- resources/js/Pages/Blog/Show/BlogPostModal.vue
+++ resources/js/Pages/Blog/Show/BlogPostModal.vue
@@ -48,10 +48,21 @@
                     <span class="material-icons z-30">share</span>
                 </button>
                 <!-- This is Disabled until it Looks better -->
-                <button v-if="false" @click="like()" class="h-16 w-16 flex rounded-full place-items-center place-content-around m-3 bg-red-700 hover:bg-red-700 shadow-lg">
+                <!--<button v-if="$page.props.user" @click="like()" class="h-16 w-16 flex rounded-full place-items-center place-content-around m-3 bg-red-700 hover:bg-red-700 shadow-lg">
                     <span class="material-icons h-16 w-16">{{isLiked?'favorite_balance':'favorite_border'}}</span>
                     {{amountOfLikes}}
+                </button>-->
+
+                <button v-if="$page.props.user" @click="like()" class="h-16 w-16  flex rounded-full place-content-around place-items-center m-3 bg-red-600 hover:bg-red-700 shadow-lg">
+                    <svg v-show="!isLiked" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>
+                    <svg v-show="isLiked" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
                 </button>
+                <div class="flex flex-row pl-5">
+                    <div class="place-self-center">
+                        {{amountOfLikes}} Likes
+                    </div>
+                </div>
+
             </div>
             <hr />
 
@@ -80,6 +91,7 @@ export default {
             showCopyMessage: false,
             isLiked: false,
             amountOfLikes: 0,
+            likeButton: '',
         }
     },
     methods:{
@@ -113,6 +125,12 @@ export default {
                     console.log(res);
                     try {
                         this.isLiked = res.data.state;
+                        if (res.data.state){
+                            this.amountOfLikes++;
+                        }
+                        else {
+                            this.amountOfLikes--;
+                        }
                     }
                     catch(e){
 
